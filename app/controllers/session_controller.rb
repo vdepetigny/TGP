b6ecc881c585be1a3b572304e96a5bb2b44658e0
vdepetigny <vdepetigny@yahoo.fr>
2019-02-07 14:24:07 +0100

    adding password

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,23 +3,11 @@ class SessionController < ApplicationController
   end
 
   def create
-  	puts "*"*70
-  	puts params
-  	puts params[:email]
-  	puts params[:password]
-
-    
     user = User.find_by(email: params[:email])
-    
-    puts "*"*70
-    puts user.first_name
-    puts user.email
-    puts user.authenticate(params[:password])
-   	puts "*"*70  
 
 	if user && user.authenticate(params[:password])
 	  session[:user_id] = user.id
-	  puts "YES"
+	  puts user.id
 	  redirect_to root_path
 	else
 	  flash.now[:danger] = 'Invalid email/password combination'
